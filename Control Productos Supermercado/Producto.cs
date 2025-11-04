203aac0e036c56daa38d0da36ffc93ee12da3e79
thetrueseros <leugimdzn@hotmail.com>
2025-11-04 12:11:07 -0500

    Clase abstracta Producto con su método abstracto CalcularPrecioFinal

---
+++ Control Productos Supermercado/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Productos_Supermercado
+{
+    public abstract class Producto
+    {
+        public string marca { get; set; }
+        public string producto { get; set; }
+        public double precioBase { get; set; }
+        public Producto(string marca, string producto, double precioBase)
+        {
+            this.marca = marca;
+            this.producto = producto;
+            this.precioBase = precioBase;
+        }
+        public abstract double CalcularPrecioFinal(double precioBase);
+    }
+}
