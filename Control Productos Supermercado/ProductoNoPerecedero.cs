c18cc1070524977a85ac1ec09ef17952c10b421a
thetrueseros <leugimdzn@hotmail.com>
2025-11-04 12:25:04 -0500

    Implementación clase ProductoNoPerecedero con su método CalcularPrecioFinal

---
+++ Control Productos Supermercado/ProductoNoPerecedero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Productos_Supermercado
+{
+    public class ProductoNoPerecedero : Producto
+    {
+        public ProductoNoPerecedero(string marca, string producto, DateTime fechaVencimiento, double precioBase) : base(marca, producto, fechaVencimiento, precioBase)
+        {
+        }
+        public override double CalcularPrecioFinal(double precioBase)
+        {
+            if (precioBase > 100000)
+            {
+                return precioBase*0.95;
+            }
+            else
+            {
+                return precioBase;
+            }
+        }
+    }
+}
