9b3154d6d6f52259119e5b40c3c7b05ebadb8c02
thetrueseros <leugimdzn@hotmail.com>
2025-11-04 12:08:00 -0500

    Agregar archivos de proyecto.

---
+++ Control Productos Supermercado/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Control_Productos_Supermercado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstVehiculos = new System.Windows.Forms.ListBox();
+            this.btnAgregarVehiculo = new System.Windows.Forms.Button();
+            this.txtbPrecioBase = new System.Windows.Forms.TextBox();
+            this.txtbProducto = new System.Windows.Forms.TextBox();
+            this.lblPrecioBase = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblSisGesVehiculos = new System.Windows.Forms.Label();
+            this.dtpFechaVence = new System.Windows.Forms.DateTimePicker();
+            this.txtbMarca = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstVehiculos
+            // 
+            this.lstVehiculos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstVehiculos.FormattingEnabled = true;
+            this.lstVehiculos.Location = new System.Drawing.Point(427, 145);
+            this.lstVehiculos.MinimumSize = new System.Drawing.Size(405, 173);
+            this.lstVehiculos.Name = "lstVehiculos";
+            this.lstVehiculos.Size = new System.Drawing.Size(422, 173);
+            this.lstVehiculos.TabIndex = 22;
+            // 
+            // btnAgregarVehiculo
+            // 
+            this.btnAgregarVehiculo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAgregarVehiculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarVehiculo.Location = new System.Drawing.Point(314, 347);
+            this.btnAgregarVehiculo.Name = "btnAgregarVehiculo";
+            this.btnAgregarVehiculo.Size = new System.Drawing.Size(230, 40);
+            this.btnAgregarVehiculo.TabIndex = 21;
+            this.btnAgregarVehiculo.Text = "Agregar Vehículo";
+            this.btnAgregarVehiculo.UseVisualStyleBackColor = true;
+            // 
+            // txtbPrecioBase
+            // 
+            this.txtbPrecioBase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtbPrecioBase.Location = new System.Drawing.Point(214, 297);
+            this.txtbPrecioBase.Name = "txtbPrecioBase";
+            this.txtbPrecioBase.Size = new System.Drawing.Size(207, 20);
+            this.txtbPrecioBase.TabIndex = 20;
+            // 
+            // txtbProducto
+            // 
+            this.txtbProducto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtbProducto.Location = new System.Drawing.Point(214, 206);
+            this.txtbProducto.Name = "txtbProducto";
+            this.txtbProducto.Size = new System.Drawing.Size(207, 20);
+            this.txtbProducto.TabIndex = 18;
+            // 
+            // lblPrecioBase
+            // 
+            this.lblPrecioBase.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPrecioBase.AutoSize = true;
+            this.lblPrecioBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecioBase.Location = new System.Drawing.Point(16, 294);
+            this.lblPrecioBase.Name = "lblPrecioBase";
+            this.lblPrecioBase.Size = new System.Drawing.Size(110, 24);
+            this.lblPrecioBase.TabIndex = 16;
+            this.lblPrecioBase.Text = "Precio base";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(16, 201);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(86, 24);
+            this.lblTipo.TabIndex = 15;
+            this.lblTipo.Text = "Producto";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarca.Location = new System.Drawing.Point(16, 151);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(62, 24);
+            this.lblMarca.TabIndex = 14;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModelo.Location = new System.Drawing.Point(16, 246);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(198, 24);
+            this.lblModelo.TabIndex = 13;
+            this.lblModelo.Text = "Fecha de vencimiento";
+            // 
+            // lblSisGesVehiculos
+            // 
+            this.lblSisGesVehiculos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSisGesVehiculos.AutoSize = true;
+            this.lblSisGesVehiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSisGesVehiculos.Location = new System.Drawing.Point(228, 64);
+            this.lblSisGesVehiculos.Name = "lblSisGesVehiculos";
+            this.lblSisGesVehiculos.Size = new System.Drawing.Size(436, 24);
+            this.lblSisGesVehiculos.TabIndex = 12;
+            this.lblSisGesVehiculos.Text = "Sistema de control de productos en supermercado";
+            // 
+            // dtpFechaVence
+            // 
+            this.dtpFechaVence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dtpFechaVence.Location = new System.Drawing.Point(221, 246);
+            this.dtpFechaVence.Name = "dtpFechaVence";
+            this.dtpFechaVence.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaVence.TabIndex = 23;
+            // 
+            // txtbMarca
+            // 
+            this.txtbMarca.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtbMarca.Location = new System.Drawing.Point(214, 151);
+            this.txtbMarca.Name = "txtbMarca";
+            this.txtbMarca.Size = new System.Drawing.Size(207, 20);
+            this.txtbMarca.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 399);
+            this.Controls.Add(this.txtbMarca);
+            this.Controls.Add(this.dtpFechaVence);
+            this.Controls.Add(this.lstVehiculos);
+            this.Controls.Add(this.btnAgregarVehiculo);
+            this.Controls.Add(this.txtbPrecioBase);
+            this.Controls.Add(this.txtbProducto);
+            this.Controls.Add(this.lblPrecioBase);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblModelo);
+            this.Controls.Add(this.lblSisGesVehiculos);
+            this.MinimumSize = new System.Drawing.Size(877, 438);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstVehiculos;
+        private System.Windows.Forms.Button btnAgregarVehiculo;
+        private System.Windows.Forms.TextBox txtbPrecioBase;
+        private System.Windows.Forms.TextBox txtbProducto;
+        private System.Windows.Forms.Label lblPrecioBase;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblSisGesVehiculos;
+        private System.Windows.Forms.DateTimePicker dtpFechaVence;
+        private System.Windows.Forms.TextBox txtbMarca;
+    }
+}
+
